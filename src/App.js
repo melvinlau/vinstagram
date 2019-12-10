306783693ad0d1ecf523955d0d44be0109cc7a41
Melvin Lau <lau.melvin@icloud.com>
2019-12-10 16:28:28 +0000

    Installed React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="row">
-      <div className="col-3">
-        <Sidebar />
+    <Router>
+      <div className="row">
+        <div className="col-3">
+          <Sidebar />
+        </div>
+        <div className="col-9">
+        </div>
       </div>
-      <div className="col-9">
-      </div>
-    </div>
+    </Router>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import { render } from '@testing-library/react';
 import { shallow } from 'enzyme';
 
 import App from './App';
 import Sidebar from './components/Sidebar';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 describe('<App />', () => {
+
+  it('renders a single <Router /> component', () => {
+    const app = shallow(<App />);
+    expect(app.find(Router).length).toBe(1);
+  });
+
   it('renders a single <Sidebar /> component', () => {
     const app = shallow(<App />);
     expect(app.find(Sidebar).length).toBe(1);
   });
+
 });
